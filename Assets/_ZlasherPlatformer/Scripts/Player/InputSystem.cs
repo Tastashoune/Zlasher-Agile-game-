15fc850ba99d2373213291dc0c6a2bd3a1a6a063
Jawad <84336004+Robenm1@users.noreply.github.com>
2025-04-08 10:28:56 +0200

    SlashAnimation added

--- Assets/_ZlasherPlatformer/Scripts/Player/InputSystem.cs
+++ Assets/_ZlasherPlatformer/Scripts/Player/InputSystem.cs
@@ -39,3 +39,59 @@ public class PlayerScript : MonoBehaviour
         }
     }
 }
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool GetJump()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    public bool GetAttack()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public float GetHorizontal()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            return 1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            return -1f;
+        }
+        return 0f;
+    }
+
+    public bool GetHorizontalReleased()
+    {
+        return Input.GetKeyUp(KeyCode.D);
+    }
+
+    public bool GetMoveLeft()
+    {
+        return Input.GetKey(KeyCode.A);
+    }
+
+    public bool GetMoveRight()
+    {
+        return Input.GetKey(KeyCode.D);
+    }
+}
