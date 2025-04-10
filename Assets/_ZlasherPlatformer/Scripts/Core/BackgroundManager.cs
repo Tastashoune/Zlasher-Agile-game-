473a87109da375260c84131ae04e6903cc0a664e
Tastashoune <after.ecall@free.fr>
2025-04-10 15:43:19 +0200

    Background1 start and material

--- Assets/_ZlasherPlatformer/Scripts/Core/BackgroundManager.cs
+++ Assets/_ZlasherPlatformer/Scripts/Core/BackgroundManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class BackgroundManager : MonoBehaviour
 {
+    public float scrollSpeed = 1f; // Vitesse de défilement de la texture
+    public GameObject backg1;
+    private Renderer rend1;
+
     void Start()
     {
-        
+        rend1 = backg1.GetComponent<Renderer>();
     }
+
     void Update()
     {
-        
+        float offset = Time.time * scrollSpeed;
+        rend1.material.mainTextureOffset = new Vector2(offset, 0f);
+        Debug.Log("Texture Offset: " + rend1.material.mainTextureOffset);
     }
 }
