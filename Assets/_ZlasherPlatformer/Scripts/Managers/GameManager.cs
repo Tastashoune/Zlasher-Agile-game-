8d93a8980b56d3bb2a71919bbe6e10500b6ba923
Tastashoune <after.ecall@free.fr>
2025-04-14 16:58:10 +0200

    First scene (StartScene)

--- Assets/_ZlasherPlatformer/Scripts/Managers/GameManager.cs
+++ Assets/_ZlasherPlatformer/Scripts/Managers/GameManager.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-  
+    private void Start()
+    {
+        
+    }
+    public void StartAfterIntro()
+    {
+        // backgrounds already OK/present in the scene
+        // floors already OK
+        // player already OK
+
+        // déclenchement du défilement du sol (starting moving floors with "LevelGenerator")
+
+        // starting moving Backgrounds
+
+        // instanciate game UI (Canvas)
+
+        // instanciate enemies spawners
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
